c9f15923de99ba4a1ac0a0387a92471c63d38fc8
Jaime Garcia Marin <jaime.garcia1@alu.uhu.es>
2025-09-29 19:13:57 +0200

    Commit 1 - Empleado

---
+++ src/Sesion2B/Empleado.java
@@ -0,0 +1,31 @@
+package Sesion2B;
+
+public class Empleado {
+	public enum TipoEmpleado{Vendedor,Encargado};
+	float salarioBase=0;
+	float primas=0;
+	float extras=0;
+	
+	public float caculoNominaBruta(TipoEmpleado tipo, float ventaMes, float HorasExtras) {
+		
+		if(tipo==TipoEmpleado.Encargado) {
+			salarioBase=2500;
+		}else if(tipo==TipoEmpleado.Vendedor) {
+			salarioBase=2000;
+		}
+		
+		if(ventaMes>=1500) {
+			primas=200;
+		}else if(ventaMes>=1000) {
+			primas=100;
+		}
+		
+		extras=HorasExtras *30;
+		
+		return salarioBase + primas + extras;
+	}
+	
+	
+	
+	
+}
