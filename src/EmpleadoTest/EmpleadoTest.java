e30d5a493ac1a86f9ffb5c96b90b910debd757c9
Jaime Garcia Marin <jaime.garcia1@alu.uhu.es>
2025-10-01 11:22:41 +0200

    Pruebas test para todas las posibilidades de nomina bruta 1	Base vendedor sin extras 2	Límite inferior sin prima 3	Límite prima +100 4	Justo antes del siguiente salto 5	Límite prima +200 6	Prima + horas extra 7	Base encargado sin extras 8	Límite inferior sin prima 9	Límite prima +100 10	Justo antes del siguiente salto 11	Límite prima +200 12	Prima + 1h extra 13	Caso general con extras altos

--- src/EmpleadoTest/EmpleadoTest.java
+++ src/EmpleadoTest/EmpleadoTest.java
@@ -7,28 +7,36 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import Sesion2B.Empleado;
+import Sesion2B.Empleado.TipoEmpleado;
 
 class EmpleadoTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+	private final Empleado emp = new Empleado();
+	
+	//Test para Nomina Bruta
+	//Lo hago parametizado para ahorrar lineas de codigo.
+    @ParameterizedTest
+    @CsvSource({
+        // tipo, 	ventas, 	horas, 	esperado
+        "Vendedor, 	0, 			0, 		2000",
+        "Vendedor, 	999, 		0, 		2000",
+        "Vendedor, 	1000, 		0, 		2100",
+        "Vendedor, 	1499, 		0,		2100",
+        "Vendedor, 	1500, 		0, 		2200",
+        "Vendedor, 	1500, 		2, 		2260",
+        "Encargado, 0, 			0, 		2500",
+        "Encargado, 999, 		0, 		2500",
+        "Encargado, 1000,		0, 		2600",
+        "Encargado, 1499, 		0, 		2600",
+        "Encargado, 1500, 		0, 		2700",
+        "Encargado, 1500, 		1, 		2730",
+        "Encargado, 2000, 		5, 		2800"
+    })
+    public void testCalculoNominaBruta(TipoEmpleado tipo, float ventas, float horas, float esperado) {
+        assertEquals(esperado, emp.caculoNominaBruta(tipo, ventas, horas), 0.01);
+    }
 }
